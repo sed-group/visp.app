2dd41279a5f9adb1fa746e99f5dda171138ab63b
Iñigo Alonso Fernandez <inigoa@net.chalmers.se>
2020-02-24 18:46:50 +0100

    added tabs

--- src/App.vue
+++ src/App.vue
@@ -19,24 +19,54 @@
       >
         <span class="mr-2">Github</span>
       </v-btn>
+      <template v-slot:extension>
+        <v-tabs 
+        v-model="tab" 
+        align-with-title
+        >
+          <v-tab key="Current">
+            Current
+          </v-tab>
+          <v-tab key="Process">
+            Process
+          </v-tab>
+          <v-tab key="Reorder">
+            Reorder
+          </v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
 
     <v-content>
-      <Reorder/>
+      <v-tabs-items v-model="tab">
+        <v-tab-item key="Current">
+          Test
+        </v-tab-item>
+        <v-tab-item key="Process">
+          <Process/>
+        </v-tab-item>
+        <v-tab-item key="Reorder">
+          <Reorder/>
+        </v-tab-item>
+      </v-tabs-items>
+      <!--<Reorder/>-->
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Process from './components/Process';
 import Reorder from './components/Reorder';
 
 export default {
   name: 'App',
   components: {
+    Process,
     Reorder,
   },
   data: () => ({
     //
+    tab: null,
   }),
 };
 </script>
